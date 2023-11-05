4aa8fe7154cf6ad70c10c6fde0741cfac0e3d1b8
BhaskarJagi <128607429+BhaskarJagi@users.noreply.github.com>
2023-11-05 22:38:34 +0530

    Updated script.js

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ const loginBtn = document.getElementById('login-btn');
 const signBtn = document.getElementById('signup-btn');
 
 loginBtn.addEventListener('click',()=>{
-    location.href='./login/index.html';
+    location.href='/login/index.html';
 })
 
 signBtn.addEventListener('click',()=>{
-    location.href='./signup/index.html'
+    location.href='/signup/index.html'
 })
